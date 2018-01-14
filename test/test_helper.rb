bc97699a248c610a3f576e98b4f228d19cc11160
Tiago Lopo Da Silva <tlopo@ibm.com>
2018-01-14 01:14:33 +0100

    Added simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,14 @@
 $LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
-require 'tlopo/executor'
 
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start
+
+require 'tlopo/executor'
 require 'minitest/autorun'
